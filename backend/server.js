bb91b72e7e8bd4f7653a9e3f0966076f7bc95471
בר קזז <barkazzaz@gmail.com>
2019-11-23 23:35:35 +0200

    socket.io initial server and client side code

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,25 @@
 const express = require("express");
+const HTTP = require("http").Server(express);
+const socketIo = require("socket.io")(HTTP);
 const cors = require("cors");
 // const mongoose = require("mongoose");
 
 require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
+
+let position = {
+    x: 200,
+    y: 200
+};
+
+socketIo.on("connection", socket =>{
+    socket.emit("position", position);
+});
 
 app.listen(port, () => {
     console.log(`server is running on port: ${port}`);
--- kazz-io/src/components/Game.component.js
+++ kazz-io/src/components/Game.component.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
+import openSocket from "socket.io-client";
+const SERVER_ADDRESS = 'http://localhost:5001';
 
 export default class Game extends Component{
+    
+    constructor(){
+        super();
+        this.state = {
+            socket = openSocket(SERVER_ADDRESS)//TODO: https://medium.com/dailyjs/combining-react-with-socket-io-for-real-time-goodness-d26168429a34
+        }
+    }
+
     render(){
         return(
             <div className="kazzContainer">
