7b17b205406ad9a253d52484fd15d687ba38a5a9
בר קזז <barkazzaz@gmail.com>
2019-11-23 19:16:29 +0200

    fixed white spaces at the page's top

--- kazz-io/src/App.js
+++ kazz-io/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import './App.css';
 import MainMenu from './components/MainMenu.component';
 import Game from './components/Game.component';
@@ -9,10 +9,13 @@ import Credits from './components/Credits.component';
 function App() {
   return (
     <Router>
-      <Route exact path="/" component={MainMenu}/>
-      <Route exact path="/credits" component={Credits}/>
-      <Route path="/room/:id" component={Game}/>
-      <Route component={DefaultComponent}/>
+      <Switch>
+        {/* Switch make only the first matching route to be displayed */}
+        <Route exact path="/" component={MainMenu}/>
+        <Route exact path="/credits" component={Credits}/>
+        <Route path="/room/:id" component={Game}/>
+        <Route component={DefaultComponent}/>
+      </Switch>
     </Router>
   );
 }
