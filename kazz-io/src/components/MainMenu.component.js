43b64c7eadad0b085b80789ed1204c8598711c46
bar.kazzaz <barkazzaz@gmail.com>
2019-11-24 05:08:50 +0200

    +createRoom +joinRoom

--- kazz-io/src/components/MainMenu.component.js
+++ kazz-io/src/components/MainMenu.component.js
@@ -3,6 +3,26 @@ import { Link } from 'react-router-dom';
 import logo from '../logo.svg';
 
 export default class MainMenu extends Component{
+
+    constructor(props){
+        super(props);
+        this.showOnclick = this.showOnclick.bind(this);
+    }
+    
+    lastClickedElem;
+    
+    showOnclick(elementId){
+        let clickedElm = document.getElementById(elementId);
+        if(this.lastClickedElem != null && this.lastClickedElem != clickedElm)
+            this.lastClickedElem.style.display = "none";
+        this.lastClickedElem = clickedElm;
+
+        if (clickedElm.style.display == "none")
+            clickedElm.style.display = "block";
+        else
+            clickedElm.style.display = "none";
+    }
+
     render(){
         return(
             <div className="kazzContainer">
@@ -18,10 +38,26 @@ export default class MainMenu extends Component{
                     </div>
                     <div className="menu">
                         <div className="btn createRoomBtn">
-                        <span>Create Room</span>
+                            <span onClick={(e) => this.showOnclick("createRoomPopup", e)}>Create Room</span>
+                            <div className="roomPopups" id="createRoomPopup" style={{display:"none"}}>
+                                <form name="createRoom" action="" method="post">
+                                    <p>Room name:
+                                        <input name="roomName" type="text"/>
+                                        <center><input name="submitCreate" type="submit" value="Create"/></center>
+                                    </p>
+                                </form>
+                            </div>
                         </div>
                         <div className="btn joinRoomBtn">
-                            <span>Join Room</span>
+                            <span onClick={(e) => this.showOnclick("joinRoomPopup", e)}>Join Room</span>
+                            <div className="roomPopups" id="joinRoomPopup" style={{display:"none"}}>
+                                <form name="joinRoom" action="" method="post">
+                                    <p>Room name:
+                                        <input name="roomName" type="text"/>
+                                        <center><input name="submitCreate" type="submit" value="Join"/></center>
+                                    </p>
+                                </form>
+                            </div>
                         </div>
                         <div className="btn showAllRoomsBtn">
                             <span>Find Room</span>
